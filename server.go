7d1f6c81456d869c610d405e71695e8d76a12fb1
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2018-01-16 21:04:21 +0200

    Added serving of barebones website

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/vuolen/reaktor-k18/db"
@@ -20,4 +21,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("SQL error: %+v\n", err)
 	}
+
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./public-html"))))
 }
